0180800d5c50f5e1d218a0f36a9cc36450b7bbc5
Nick Sergeev <nick555@bk.ru>
2019-11-13 18:01:22 +0300

    my commit #11

---
+++ my_lesson_loops.py
@@ -0,0 +1,29 @@
+for x in range(10,10+2,2):
+    print(x)
+    print("----")
+    print("Number x = " + str (x))
+
+
+
+for x in range(10, 0, -2):
+    print(x)
+    print("----")
+    print("Number x = " + str (x))
+    if x == 6:
+        break
+
+print("--------------------EOF---------------------------")
+
+# Бесконечный цикл
+x=1
+while True:
+    print(x)
+    x = x + 1
+
+# Бесконечный цикл
+x=1
+while True:
+    print(x)
+    x = x + 1
+    if x == 100000
+        break
--- my_lesson_number.py
+++ my_lesson_number.py
@@ -1,1 +1,8 @@
-t =
+num1 = 30
+num2 = 40
+
+print(num1+num2)
+print(num1/10)
+print(num2/3)
+
+print(100+4/2-1+(2+5))
