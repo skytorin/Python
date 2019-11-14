7406e873b22fb9003e8c07a04a9b3ba6fdbe4b03
Nick Sergeev <nick555@bk.ru>
2019-11-14 16:14:46 +0300

    my commit #14

---
+++ my_lesson_10_if_elif_else.py
@@ -0,0 +1,34 @@
+# Проверка введеного значения
+x = 25
+if x == 25:
+    print("YES you RIGNT")
+else:
+    print("NO you are WRONG")
+
+# Проверка введеного возраста с указанием на принадлежность возрасной группе
+age = 19
+if (age <= 4):
+    print("Yoa are BABY")
+elif (age > 4) and (age < 12):
+    print("You are KID")
+elif (age >=12) and (age < 19):
+    print("You are TEENAGER")
+else:
+    print("You are old")
+
+# Проверка есть ли указанная машина в масссиве
+all_cars = ['chrusler','dacia','bmv','KIA','vw','seat','skoda','lada','audi','ford','chevrolett']
+german_cars =  ['bmv','vw','audi']
+if "lada" in all_cars:
+    print("Yes LADA is in the list")
+else:
+    print("LADA Not in the list")
+
+# Поиск в массиве только германских авто
+all_cars = ['chrusler', 'dacia', 'bmv', 'KIA', 'vw', 'seat', 'skoda','lada', 'audi', 'ford', 'chevrolett']
+german_cars =  ['bmv', 'vw', 'audi']
+for xxxx in all_cars:
+    if xxxx in german_cars:
+        print(xxxx + " In German Car")
+    else:
+        print(xxxx + " Is NOT German Car")
