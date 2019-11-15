3fd9a7efcc50f6d9124a38677ac7deda7875c1da
Nick Sergeev <nick555@bk.ru>
2019-11-15 17:23:01 +0300

    Edit My_Lesson_02_String.py (15.11.2019 17:21)

---
+++ My_Lesson_02_String.py
@@ -0,0 +1,14 @@
+# Работа со строками
+name = "vasya"
+sername = "pupkin"
+stroka = "Bla Bla Bla"
+
+print("name\n sername\n stroka")
+print(name.title()+ "\n" +sername.upper()+"\n\n" +stroka)
+print("------------------------------------------------------")
+
+a = " ...... dadyaVasya ,,,,, "
+a = a.strip()
+a = a.rstrip(",")
+a = a.lstrip(".")
+print(a.strip())
