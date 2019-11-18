2b5b6d58aa3009166ded4f6ff481788ab89575d3
Nick Sergeev <nick555@bk.ru>
2019-11-18 17:25:23 +0300

    Edit My_Lesson_15_Functions_2.py (18.11.2019 17:25)

--- My_Lesson_15_Functions_2.py
+++ My_Lesson_15_Functions_2.py
@@ -1,3 +1,32 @@
+# Создание функции для заболнения библиотеки
+def create_record(name, telephone, address):
+    """Create Record"""
+    record = {
+        'name': name,
+        'telephone': telephone,
+        'address': address
+    }
+    return  record
+
+user1 = create_record("Vasya", "+7955123456789", "Moscow")
+user2 = create_record("Petya", "+7944987654321", "London")
+
+print(user1)
+print(user2)
+
+
+# Функция с возмозностью указания произвольного кол-ва персон, заранее не определенных (указывается в конце определения)
+def give_award(medal, *persons):
+    """Give Medal to persons"""
+    for person in persons:
+        print("Tovarish " + person.title() + " nagrajdaetsya medaliyu " + medal)
+
+give_award("Za Berlin", "Vasya", "petya")
+give_award("Za London", "petya", "alexander", "Valentin")
+
+
+
+
 
 
 
