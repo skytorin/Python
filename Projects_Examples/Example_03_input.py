596b5398f56a72cd20a8b90768ee2f946ff79b0f
nick <nick555@bk.ru>
2021-10-11 23:55:16 +0300

    Editet 11.10.2021 20:54:00

--- Projects_Examples/Example_03_input.py
+++ Projects_Examples/Example_03_input.py
@@ -1,4 +1,4 @@
-
+#! /usr/bin/env python3
 # Запись в массив всех введенных данных поьзователя с выходом при вводе ключевого слова STOP
 mylist = []
 msg = ""
@@ -6,3 +6,4 @@ while msg != 'stop'.upper():
     msg = input("Enter new item, and STOP to finish: ")
     mylist.append(msg)
 print(mylist)
+print(mylist[0-3])
