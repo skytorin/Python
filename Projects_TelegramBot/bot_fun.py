aca3abd6a63a66a8545086df95ba24088d9cb7c5
nick <nick555@bk.ru>
2022-10-22 15:58:09 +0000

    Added Telegram Bot

---
+++ Projects_TelegramBot/bot_fun.py
@@ -0,0 +1,15 @@
+import telebot
+import time
+
+bot = telebot.TeleBot('telegram_token')
+CHANNEL_NAME = '@адрес_telegram_канала'
+# Загружаем список шуток
+f = open('data/fun.txt', 'r', encoding='UTF-8')
+jokes = f.read().split('\n')
+f.close()
+
+for joke in jokes:
+    bot.send_message(CHANNEL_NAME, joke)
+    # Делаем паузу в один час
+    time.sleep(3600)
+bot.send_message(CHANNEL_NAME, "Шутки закончились :-(")
